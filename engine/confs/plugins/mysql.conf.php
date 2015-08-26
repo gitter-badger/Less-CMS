0b07ff09682597677aff18952edf03b77ade36b0
N3stY <n3sty@outlook.com>
2015-08-26 13:46:58 +0200

    3.7.6

--- engine/confs/plugins/mysql.conf.php
+++ engine/confs/plugins/mysql.conf.php
@@ -1,13 +1,13 @@
 <?php
-if (!defined('l8xZZV6AZqVoF91XM7')){require $_SERVER['DOCUMENT_ROOT'] . "/engine/errors.php"; exit;}
+if (!defined('{security_code}')){require $_SERVER['DOCUMENT_ROOT'] . "/engine/errors.php"; exit;}
 
 
-define ("MYSQL_HOST", "localhost"); 
+define ("MYSQL_HOST", "localhost");
 
 define ("MYSQL_NAME", "cherry_db");
 
 define ("MYSQL_USER", "cherry_user");
 
 define ("MYSQL_PASS", "");
- 
+
 ?>
